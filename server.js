9e3d3d5f3bc76f41f43bf34ee4e8e104112c887e
Aarush Chaudhary <aarushchaudhary153@gmail.com>
2026-04-07 00:38:42 +0530

    Simplify static file serving - remove duplicate routes

--- server.js
+++ server.js
@@ -18,24 +18,11 @@ app.use((req, res, next) => {
 });
 
 const publicPath = path.join(__dirname, 'public');
-app.use(express.static(publicPath, { 
-    setHeaders: (res, path) => {
-        if (path.endsWith('.js')) {
-            res.setHeader('Content-Type', 'application/javascript');
-        } else if (path.endsWith('.css')) {
-            res.setHeader('Content-Type', 'text/css');
-        }
-    }
-}));
+
+app.use(express.static(publicPath));
 
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
-
-app.use('/angular-1.8.2', express.static(path.join(publicPath, 'angular-1.8.2')));
-app.use('/css', express.static(path.join(publicPath, 'css')));
-app.use('/js', express.static(path.join(publicPath, 'js')));
-app.use('/views', express.static(path.join(publicPath, 'views')));
-app.use('/assets', express.static(path.join(publicPath, 'assets')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
