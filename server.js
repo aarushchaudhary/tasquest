df1b2cb3d99bd5b8214901bbb171ea30e7800d04
Aarush Chaudhary <aarushchaudhary153@gmail.com>
2026-04-07 00:29:38 +0530

    Added vercel.json

--- server.js
+++ server.js
@@ -4,13 +4,33 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(express.static('public'));
 
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`TasQuest Server is running on http://localhost:${PORT} ⚔️`);
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 });
+
+module.exports = app;
+
+if (!process.env.VERCEL) {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`TasQuest Server is running on http://localhost:${PORT} ⚔️`);
+    });
+}
